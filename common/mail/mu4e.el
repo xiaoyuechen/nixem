ec6a7b90726997cc1da1d91b9e18f219fa7bc383
Xiaoyue Chen <xchen@vvvu.org>
2023-11-13 00:08:47 +0100

    Add licence header to mu4e.el

--- common/mail/mu4e.el
+++ common/mail/mu4e.el
@@ -1,3 +1,28 @@
+;;; mu4e.el --- Mu4e configurations                  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Xiaoyue Chen
+
+;; Author: Xiaoyue Chen <xchen@vvvu.org>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (use-package mu4e
   :ensure t
   :bind
@@ -114,3 +139,5 @@
           (mml-secure-sign))))))
 
   (setq mml-secure-openpgp-sign-with-sender t))
+
+;;; mu4e.el ends here
