d7a4dff11223a5e84e31de3f06b2b85a7b37757d
Xiaoyue Chen <xchen@vvvu.org>
2023-11-15 23:29:25 +0100

    Update documentation

--- flake.nix
+++ flake.nix
@@ -1,5 +1,20 @@
+# Copyright (C) 2023  Xiaoyue Chen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 {
-  description = "NixOS configurations for a home network";
+  description = "nixem --- modular NixOS configurations for my personal devices";
 
   inputs = {
     emacs-overlay = {
